a209ea0259b432fc80d9347f08fd9abf0964a516
Madeleine Barr <madeleine.barr13@gmail.com>
2021-03-23 11:40:10 -0400

    added route to work page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
         <div className="menu">
         <ul>
             <li><a href="/badeleinemWebsite/">About</a></li>
-            <li><a href="/work">Work</a></li>
+            <li><a href="/badeleinemWebsite/work">Work</a></li>
             <li><a href="/books">Books</a></li>
             {/* <li><a href="/topics">Topics</a></li> */}
             <li><a href="/art">Art</a></li>
